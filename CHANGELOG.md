feaf998cd5afad7b4181c491d4453d68aeaccb51
joanpaon <joanpaon@gmail.com>
2017-05-23 19:23:01 +0200

    Version 0.1.0 - :tada:Commit Inicial

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+<pre>
+
+Version 0.1.0
+=============
+
+Info
+----
+Date ....: 23/05/2017
+---
+
+Added
+-----
+> None
+---
+
+Updated
+-------
+> README.md
+> CHANGELOG.md
+> org.japo.java.forms.GUI.java
+> org.japo.java.events.AEM.java
+---
+
+Deleted
+-------
+> None
+---
+
+</pre>
---
+++ src/org/japo/java/forms/GUI.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2017 José A. Pacheco Ondoño - joanpaon@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.forms;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import org.japo.java.events.AEM;
+
+/**
+ *
+ * @author José A. Pacheco Ondoño - joanpaon@gmail.com
+ */
+public class GUI extends JFrame {
+
+    // Campos de Hora
+    private int hor;
+    private int min;
+
+    // Referencias a los Componentes
+    JLabel lblDisplay;
+    JRadioButton rbtHor;
+    JRadioButton rbtMin;
+    JButton btnMas;
+    JButton btnMen;
+
+    // Constructor
+    public GUI() {
+        // Inicializar GUI - PREVIA
+        beforeInit();
+
+        // Construcción - GUI
+        initComponents();
+
+        // Inicializar GUI - POSTERIOR
+        afterInit();
+    }
+
+    // Inicializar GUI - PREVIA
+    private void beforeInit() {
+
+    }
+
+    // Inicializar GUI - POSTERIOR
+    private void afterInit() {
+
+    }
+
+    // Construcción - GUI
+    private void initComponents() {
+        // Fuentes
+        Font fntLBL = new Font("Courier", Font.PLAIN, 100);
+        Font fntCTR = new Font("Consolas", Font.PLAIN, 26);
+
+        // Colores
+        Color c = new Color(184, 244, 244);
+
+        // Bordes
+        BevelBorder brdLBL = new BevelBorder(BevelBorder.LOWERED);
+        EmptyBorder brdPNL = new EmptyBorder(10, 10, 10, 10);
+
+        // Gestor de Eventos de acción
+        AEM aem = new AEM(this);
+
+        // Display
+        lblDisplay = new JLabel("00:00");
+        lblDisplay.setFont(fntLBL);
+        lblDisplay.setOpaque(true);
+        lblDisplay.setBackground(c);
+        lblDisplay.setBorder(brdLBL);
+        lblDisplay.setHorizontalAlignment(JLabel.CENTER);
+
+        // Seleccion - Horas
+        rbtHor = new JRadioButton("Horas");
+        rbtHor.setFont(fntCTR);
+        rbtHor.setSelected(true);
+
+        // Seleccion - Minutos
+        rbtMin = new JRadioButton("Minutos");
+        rbtMin.setFont(fntCTR);
+        rbtMin.setSelected(true);
+
+        // Coordinador Botones
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(rbtHor);
+        bg.add(rbtMin);
+
+        // Botón - MAS
+        btnMas = new JButton("+");
+        btnMas.setFont(fntCTR);
+        btnMas.addActionListener(aem);
+
+        // Botón - MENOS
+        btnMen = new JButton("-");
+        btnMen.setFont(fntCTR);
+        btnMen.addActionListener(aem);
+
+        // Panel Seleccion
+        JPanel pnlSelector = new JPanel();
+        pnlSelector.setBorder(brdPNL);
+        pnlSelector.setLayout(new GridLayout(2, 1));
+        pnlSelector.add(rbtHor);
+        pnlSelector.add(rbtMin);
+
+        // Panel de ajuste
+        JPanel pnlAjuste = new JPanel();
+        pnlAjuste.setBorder(brdPNL);
+        pnlAjuste.setLayout(new GridLayout(1, 2));
+        pnlAjuste.add(btnMas);
+        pnlAjuste.add(btnMen);
+
+        // Panel de control
+        JPanel pnlControl = new JPanel();
+        pnlControl.setBorder(brdPNL);
+        pnlControl.setLayout(new GridLayout(1, 2));
+        pnlControl.add(pnlSelector);
+        pnlControl.add(pnlAjuste);
+
+        // Panel Principal
+        JPanel pnlPpal = new JPanel();
+        pnlPpal.setBorder(brdPNL);
+        pnlPpal.setLayout(new GridLayout(2, 1));
+        pnlPpal.add(lblDisplay);
+        pnlPpal.add(pnlControl);
+
+        // Icono Ventana - Recurso
+        URL urlICN = getClass().getResource("/img/favicon.png");
+        Image imgICN = new ImageIcon(urlICN).getImage();
+
+        // Ventana principal
+        setTitle("Ajuste Hora");
+        setContentPane(pnlPpal);
+        setResizable(false);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setIconImage(imgICN);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void procesarAccion(ActionEvent e) {
+        if (e.getSource().equals(btnMas)) {
+            if (rbtHor.isSelected()) {
+                if (hor < 23) {
+                    hor++;
+                } else {
+                    hor = 0;
+                }
+            } else {
+                if (min < 59) {
+                    min++;
+                } else {
+                    min = 0;
+                }
+            }
+        } else {
+            if (rbtHor.isSelected()) {
+                if (hor > 0) {
+                    hor--;
+                } else {
+                    hor = 23;
+                }
+            } else {
+                if (min > 0) {
+                    min--;
+                } else {
+                    min = 59;
+                }
+            }
+        }
+
+        // Mostrar display
+        lblDisplay.setText(String.format("%02d:%02d", hor, min));
+    }
+}
